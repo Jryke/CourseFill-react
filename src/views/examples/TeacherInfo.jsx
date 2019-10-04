415adae2d905013c577a78cfb669944c1436f367
Jryke <rykerson@gmail.com>
2019-10-04 22:25:45 +0700

    code cleaned up

--- src/views/examples/TeacherInfo.jsx
+++ src/views/examples/TeacherInfo.jsx
@@ -1,19 +1,13 @@
 /*!
-
 =========================================================
 * Argon Dashboard React - v1.0.0
 =========================================================
-
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
 * Copyright 2019 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
 * Coded by Creative Tim
-
 =========================================================
-
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 */
 import React from "react";
 
@@ -238,9 +232,7 @@ class TeacherInfo extends React.Component {
 		                      Students
 		                    </h6>
 												{/*
-
 		                    <div className="pl-lg-4">
-
 													{
 														this.state.students.map((student, key) => {
 															return(
@@ -529,9 +521,7 @@ class TeacherInfo extends React.Component {
 		                      Students
 		                    </h6>
 												{/*
-
 		                    <div className="pl-lg-4">
-
 													{
 														this.state.students.map((student, key) => {
 															return(
@@ -587,10 +577,8 @@ class TeacherInfo extends React.Component {
 		        </Container>
 					)
 				}
-
       </>
     );
   }
 }
-
 export default TeacherInfo;
