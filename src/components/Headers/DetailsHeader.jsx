04495f68c67f76d018109c95b27b59694d1548f8
Jryke <rykerson@gmail.com>
2019-10-05 11:02:29 +0700

    Lorem ipsum placeholder removed from description

--- src/components/Headers/DetailsHeader.jsx
+++ src/components/Headers/DetailsHeader.jsx
@@ -44,8 +44,6 @@ class DetailsHeader extends React.Component {
 								<h2 className="display-4 text-white">{this.props.subtitle}</h2>
                 <p className="text-white mt-0 mb-5">
 									{this.props.info}
-									<br />
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
               </Col>
             </Row>
