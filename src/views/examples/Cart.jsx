1d04b182ec16700fefcb8e2ac9eaadbd91feed12
Jryke <rykerson@gmail.com>
2019-10-31 23:40:56 -0400

    refactored and cleaned up, conditional for empty cart added

--- src/views/examples/Cart.jsx
+++ src/views/examples/Cart.jsx
@@ -24,32 +24,43 @@ class Cart extends React.Component {
 		cart: [],
 		total: ''
 	}
-//
-//
-//
-//
-componentDidMount() {
-	this.setState({cart: JSON.parse(localStorage.getItem('cart'))})
-	console.log(this.state.cart)
-}
-
-removeItem = (e, course) => {
-	e.preventDefault()
-	let index = this.state.cart.findIndex(el => el.name === course.name)
-	let cartArr = this.state.cart
-	cartArr.splice(index, 1)
-	let cart = this.state.cart
-	localStorage.setItem('cart', JSON.stringify(cart))
-	this.setState({cart})
-}
-
+	componentDidMount() {
+		let cart = JSON.parse(localStorage.getItem('cart'))
+		this.setState({cart})
+	}
+	renderItems = () => {
+		if (this.state.cart) {
+			return this.state.cart.map((course, key) => {
+				return (
+					<div className="text-center text-muted mb-4" key={key}>
+						Course: {course.name} 
+						<span style={{ marginLeft: "1rem" }}> 
+							Price: ${course.price}
+						</span>
+						<span>
+							<Button size="sm" color="primary" onClick={e => this.removeItem(e, course.name )} style={{ marginLeft: "1rem" }}>
+								Remove Course
+							</Button>
+						</span>
+					</div>
+				)
+			})
+		}
+	}
+	renderTotal = () => this.state.cart ? this.state.cart.reduce((a, c) => a + c.price, 0) : 0
+	removeItem = (e, course) => {
+		e.preventDefault()
+		let index = this.state.cart.findIndex(el => el.name === course.name)
+		let cartArr = this.state.cart
+		cartArr.splice(index, 1)
+		let cart = this.state.cart
+		localStorage.setItem('cart', JSON.stringify(cart))
+		this.setState({cart})
+	}
 	clearCart = () => {
 		 localStorage.removeItem('cart');
 		 this.setState({cart: []});
 	 }
-
-
-//
 	render () {
 		return (
 			<>
@@ -61,33 +72,20 @@ removeItem = (e, course) => {
             </CardHeader>
             <CardBody className="px-lg-5 py-lg-5">
               <div className="text-center text-muted mb-4">
-
-									{(this.state.cart.map((course, key) => {
-										return (<div className="text-center text-muted mb-4" key={key}>{course.name} <span style={{ marginLeft: "1rem" }}> ${course.price}</span>
-										<span>
-											<Button size="sm" color="primary" onClick={e => this.removeItem(e, course.name )
-												}
-										style={{ marginLeft: "1rem" }}>Remove Course</Button></span></div>
-									)}))}
-
-									<div className="text-center text-heavy mb-4">Total:	${(this.state.cart.reduce((a, c) =>
-										a + c.price, 0))}</div>
-
-
-									<Button size="sm" color="danger" onClick={this.clearCart}
-									style={{ marginRight: "10px" }}>Clear Cart</Button>
-
-
+								{this.renderItems()}
+								<div className="text-center text-heavy mb-4">
+									Total:	${this.renderTotal()}
+								</div>
+								<Button size="sm" color="danger" onClick={this.clearCart} style={{ marginRight: "10px" }}>
+									Clear Cart
+								</Button>
               </div>
-
             </CardBody>
-
-							<StripeProvider apiKey="pk_test_P55aXLui6UUIKIktSJYLq56p00uE4eoJif">
-								<Elements>
-									<Checkout total={(this.state.cart.reduce((a, c) =>
-										a + c.price, 0))}/>
-								</Elements>
-							</StripeProvider>
+						<StripeProvider apiKey="pk_test_P55aXLui6UUIKIktSJYLq56p00uE4eoJif">
+							<Elements>
+								<Checkout total={this.renderTotal()} />
+							</Elements>
+						</StripeProvider>
           </Card>
         </Col>
       </>
