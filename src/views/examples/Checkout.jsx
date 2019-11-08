7dfb9da96516b19c9af9fea1fb98769653c9317f
Jryke <rykerson@gmail.com>
2019-11-08 16:28:43 -0500

    info sending to db, adding student to class and class to student

--- src/views/examples/Checkout.jsx
+++ src/views/examples/Checkout.jsx
@@ -11,24 +11,52 @@ class Checkout extends React.Component {
 			axios.post(`${process.env.REACT_APP_API_PORT}/pay`, {
 				amount: this.props.total * 100,
 				currency: 'usd',
-				description: res.body.name,
+				description: 'Course Registration',
 				source: res.token.id
-			}).then(res => {
-				alert('Payment successful')
-			}).then(res => {
-				window.location.assign('https://media.giphy.com/media/3oeSALRyH4rYS3Nt8Q/giphy.gif');
 			})
-		}).catch(err => console.log(err))
+			.then(res => {
+				alert('Payment successful')
+			})
+			.then(res => {
+				let courses = this.props.user.courses
+				this.props.cart.forEach(course => {
+					if (!courses.includes(course._id)) {
+						courses.push(course._id)
+					}
+				})
+				axios.patch(`${process.env.REACT_APP_API_PORT}/students/${this.props.user._id}`, {
+					courses: courses
+				})
+			})
+			.then(res => {
+				axios.all(
+					this.props.cart.map(course => {
+						return (
+							axios.patch(`${process.env.REACT_APP_API_PORT}/courseAddStudent/${course._id}`, {
+							students: this.props.user._id
+							})
+						)
+					})
+				)
+			})
+			.then(res => {
+				localStorage.removeItem("cart")
+			})
+		})
+		.catch(err => {
+			alert('Please complete payment information')
+			console.log(err)
+		})
 	}
 
 	render () {
-	return (
-		<>
-		<CardElement />
-			<Button onClick={this.submit} className="btn btn-success" size="sm">Checkout</Button>
+		return (
+			<>
+				<CardElement />
+				<Button onClick={this.submit} className="btn btn-success" size="sm">Checkout</Button>
 			</>
-  )
-}
+		)
+	}
 }
 
 export default injectStripe(Checkout)
