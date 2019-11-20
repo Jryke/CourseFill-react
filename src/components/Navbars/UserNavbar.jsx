0bf3e93764e0e75fe9559d5636d77ad0eef35ab1
Jryke <rykerson@gmail.com>
2019-11-20 13:48:48 -0500

    renderDropDownOption added

--- src/components/Navbars/UserNavbar.jsx
+++ src/components/Navbars/UserNavbar.jsx
@@ -46,6 +46,23 @@ class UserNavbar extends React.Component {
       )
     }
   }
+  renderDropDownOption = () => {
+    if (this.props.match.path === "/student") {
+      return(
+        <DropdownItem to="../../auth/cart" tag={Link}>
+          <i className="ni ni-cart" />
+          <span>Cart</span>
+        </DropdownItem>
+      )
+    } else if (this.props.user.role === "admin") {
+      return(
+        <DropdownItem to="/admin/user-profile" tag={Link}>
+          <i className="ni ni-settings-gear-65" />
+          <span>Settings</span>
+        </DropdownItem>
+      )
+    }
+  }
   logout = () => {
     localStorage.removeItem("token")
     this.props.history.push({
@@ -96,22 +113,21 @@ class UserNavbar extends React.Component {
                   <DropdownItem className="noti-title" header tag="div">
                     <h6 className="text-overflow m-0">Welcome!</h6>
                   </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <DropdownItem to={`/admin/teacher/${this.props.user._id}`} tag={Link}>
                     <i className="ni ni-single-02" />
                     <span>My profile</span>
                   </DropdownItem>
                   <DropdownItem to="/admin/user-profile" tag={Link}>
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Settings</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-calendar-grid-58" />
-                    <span>Activity</span>
+                    <span>Schedule</span>
                   </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" tag={Link}>
+                  {/* <DropdownItem to="/admin/user-profile" tag={Link}>
                     <i className="ni ni-support-16" />
                     <span>Support</span>
-                  </DropdownItem>
+                  </DropdownItem> */}
+                  {
+                    this.renderDropDownOption()
+                  }
                   <DropdownItem divider />
                   <DropdownItem onClick={this.logout}>
                     <i className="ni ni-user-run" />
